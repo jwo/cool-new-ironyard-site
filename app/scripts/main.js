968e40d30a63e2a6ba885ecde23139be4683d521
FlackAP <flack.ap@gmail.com>
2014-05-12 14:17:54 -0400

    added wpFeed() to main.js. do somethin' widdit

---
+++ app/scripts/main.js
@@ -0,0 +1,21 @@
+function wpFeed(url, $el) {
+     $.ajax({
+       url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=10&num=10&callback=?&q=' + encodeURIComponent(url),
+       dataType: 'json',
+       success: function(data) {
+         
+         var entries = data.responseData.feed.entries;
+         var fragment = '';
+         
+         if(entries) {
+            
+            for(var i=0;i < 4; i++) {
+             fragment += '<li><a href="'+entries[i].link+'">'+entries[i].title+'</a></li>';
+             
+            }
+           
+           $el.append(fragment);
+           }
+         }
+     });
+}
