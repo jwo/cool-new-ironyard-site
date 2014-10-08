604110119f6991651c4741890e66ed00d539ac2d
FlackAP <flack.ap@gmail.com>
2014-10-08 17:54:39 -0400

    this is wrong

--- app/scripts/graduates.js
+++ app/scripts/graduates.js
@@ -4,6 +4,9 @@ console.log('loaded!');
 $(document).ready(function() {
   $('.filter-item').click(function(e){
     $('.' + e.currentTarget.id).toggle();
+    if($('.' + e.currentTarget.id).length == 0) {
+      $('.' + e.currentTarget.id).toggle();
+    }
   });
 
 });
