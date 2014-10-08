016b7351b4c80fe0e3137f103684d809abd736c7
FlackAP <flack.ap@gmail.com>
2014-10-08 19:42:22 -0400

    this is also wrong

--- app/scripts/graduates.js
+++ app/scripts/graduates.js
@@ -3,10 +3,7 @@ console.log('loaded!');
 
 $(document).ready(function() {
   $('.filter-item').click(function(e){
-    $('.' + e.currentTarget.id).toggle();
-    if($('.' + e.currentTarget.id).length == 0) {
-      $('.' + e.currentTarget.id).toggle();
-    }
+    $('.' + e.currentTarget.id).toggleClass('hide');
   });
 
 });
