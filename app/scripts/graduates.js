81d2c5f38462eeb45e98fb271b771da11d1519df
Andy Flack <flack.ap@gmail.com>
2014-10-08 13:22:10 -0400

    WAT

--- app/scripts/graduates.js
+++ app/scripts/graduates.js
@@ -2,8 +2,17 @@
 console.log('loaded!');
 
 $(function() {
+
+  function filterGrads(select, target) {
+    $(select).click(function() {
+      console.log(select ' clicked!');
+      $(target).toggle();
+    })
+  }
+
   $('#greenville').click(function() {
     console.log('greenville clicked!!');
     $('.greenville').toggle();
   });
+
 });
