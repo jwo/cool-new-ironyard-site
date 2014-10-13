d65166eeaebb4ba06cf1214d0fd25e956260a2f0
Andy Flack <flack.ap@gmail.com>
2014-10-13 12:09:28 -0400

    this is going to have to work for now

--- app/scripts/graduates.js
+++ app/scripts/graduates.js
@@ -10,5 +10,21 @@ $(document).ready(function() {
     $(filteredDiv).toggleClass('hide');
   });
 
+  $('.clear-all').click(function(){
+    $('.filter-item').each(function(){
+      $(this).removeClass('gold-text');
+    });
+      
+    $('.bio').addClass('hide');
+  })
+
+  $('.show-all').click(function(){
+    $('.filter-item').each(function(){
+      $(this).addClass('gold-text');
+    });
+      
+    $('.bio').removeClass('hide');
+  })
+
 });
 
