f9ed9333762eef8ae415add65c44fb9dd7d2a125
FlackAP <flack.ap@gmail.com>
2014-10-31 15:14:08 -0400

    removed graduates json fileand pointless javascript

--- app/scripts/graduates.js
+++ app/scripts/graduates.js
@@ -28,17 +28,3 @@ $(document).ready(function() {
 
 });
 
-var request = new XMLHttpRequest();
-request.open("GET", "../../../graduates.json", false);
-request.send(null);
-request.parseJSON = function() {
-  if ( request.readyState === 4 && request.status === 200 ) {
-    var my_JSON_object = JSON.parse(request.responseText);
-    console.log(my_JSON_object);
-  }
-  else {
-    console.log("didn't work")
-  }
-}
-request.parseJSON();
-
